9e15bd8a79fb77a7e8f9d0cb8d797e289a864d59
t_skorbilina <t.skorbilina@pay-lab.ru>
2025-01-03 23:21:54 +0300

    initial commit

---
+++ core/src/main/java/com/example/core/navigation/DestinationMainContent.kt
@@ -0,0 +1,7 @@
+package com.example.core.navigation
+
+object DestinationMainContent {
+    const val CREATE_RECIPE_ROUTE = "create_recipe"
+    const val FIND_ROUTE = "find_recipe"
+    const val BOOK_ROUTE = "recipe_book"
+}
---
+++ feature_find/src/main/java/com/example/feature_find/FindRoute.kt
@@ -0,0 +1,116 @@
+package com.example.feature_find
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun FindRoute(
+    nameScreen: String,
+    // coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    //charactersViewModel: CharactersViewModel = hiltViewModel()
+) {
+    var searchQuery by remember { mutableStateOf("") }
+    var searchResults by remember { mutableStateOf<List<String>>(emptyList()) }
+
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xFF6200EE))
+            .padding(16.dp)
+    ) {
+        Text(
+            text = nameScreen,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.CenterStart)
+        )
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 16.dp, end = 16.dp) // Паддинг для отступов слева и справа
+            .padding(top = 80.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        // Поле ввода для поиска
+        OutlinedTextField(
+            value = searchQuery,
+            onValueChange = { searchQuery = it },
+            label = { Text("Поиск рецептов") },
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true
+        )
+
+        // Кнопка для поиска
+        Button(
+            onClick = {
+                // Логика поиска
+                searchResults = performRecipeSearch(searchQuery)
+            },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text("Поиск")
+        }
+
+        // Таблица с результатами поиска
+        if (searchResults.isNotEmpty()) {
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(searchResults) { result ->
+                    RecipeItem(recipeName = result)
+                }
+            }
+        } else {
+            Text("Нет результатов", style = MaterialTheme.typography.bodyLarge)
+        }
+    }
+}
+
+@Composable
+fun RecipeItem(recipeName: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+           // .shadow(4.dp, shape = RoundedCornerShape(8.dp)), // Добавление тени
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = recipeName,
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+// Функция для имитации поиска рецептов (например, получение из API или локальной базы данных)
+fun performRecipeSearch(query: String): List<String> {
+    // Здесь логика для поиска. Например, фильтрация по запросу или получение из API.
+    val allRecipes = listOf("Cake","Борщ", "Суп из тыквы", "Пицца", "Торт Наполеон", "Паста карбонара")
+    return if (query.isBlank()) {
+        emptyList()
+    } else {
+        allRecipes.filter { it.contains(query, ignoreCase = true) }
+    }
+}
+
+
